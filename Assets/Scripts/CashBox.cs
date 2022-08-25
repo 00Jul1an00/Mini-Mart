b2fdff20ff6c91336b2e5d85cf2dd959afd93b18
Bonifa32 <Salmon3221@mail.ru>
2022-08-26 02:19:54 +0300

    11

--- Assets/Scripts/CashBox.cs
+++ Assets/Scripts/CashBox.cs
@@ -4,4 +4,57 @@ using UnityEngine;
 
 public class CashBox : MonoBehaviour
 {
+    [SerializeField] private GameObject _playerZone;
+    public Transform CashTransform;
+    private Vector3 _moneyTakeDistance = new Vector3(2,0,2);
+    private bool _isCashierBought;
+    public int Money { get; private set; }
+    private bool _isCustomerServed;
+
+    private void Start()
+    {
+        CashTransform = GetComponent<Transform>();
+    }
+    private IEnumerator ServingClient()
+    {
+        yield return new WaitForSeconds(3);
+    }
+    private void Update()
+    {
+        print(_isCustomerServed);
+    }
+    private bool OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<PlayerMover>(out PlayerMover player))
+        {
+            StartCoroutine(ServingClient());
+            return _isCustomerServed = true;           
+        }      
+        else
+        {
+            return _isCustomerServed = false;
+        }
+
+    }
+    public void MoneySetter(int money, object objectChanger)
+    {
+        if (objectChanger is MoveToCashBoxState && money > 0)
+        {
+            Money += money;
+        }
+    }
+
+    private void TakeMoney()
+    {
+        if(Vector3.Distance(PlayerMover.PlayerTransform.position, CashTransform.position) < _moneyTakeDistance)
+        {
+
+        }
+
+        
+    }
+
+    
+
+    
 }
--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -9,18 +9,19 @@ public class PlayerMover : MonoBehaviour
     private CharacterController _chController;
     private Vector3 _directionVector;
     private float _playerSpeed = 5f;
-    private Transform _playerTransform;
+    public static Transform PlayerTransform;
+    
     void Start()
     {
         _rigidBody = GetComponent<Rigidbody>();
         _chController = GetComponent<CharacterController>(); 
-        _playerTransform = GetComponent<Transform>(); 
-        
+        PlayerTransform = GetComponent<Transform>(); 
+       
     }
     
     void Update()
     {
-        Movement();
+        Movement();        
     }
 
     private void Movement()
@@ -28,16 +29,19 @@ public class PlayerMover : MonoBehaviour
         _directionVector.x = Input.GetAxis("Horizontal") * _playerSpeed;
         _directionVector.z = Input.GetAxis("Vertical") * _playerSpeed;
         _chController.Move(_directionVector * Time.deltaTime);
-        if (_playerTransform.transform.hasChanged == true)
-            _particleSystem.Play();
+        
+        
             
-
-            
-
-        if (Vector3.Angle(Vector3.forward, _directionVector) >1 || Vector3.Angle(Vector3.forward, _directionVector) == 0)
+        if (Vector3.Angle(Vector3.forward, _directionVector) > 1 || Vector3.Angle(Vector3.forward, _directionVector) == 0)
         {
             Vector3 direct = Vector3.RotateTowards(transform.forward, _directionVector, _playerSpeed, 0f);
             transform.rotation = Quaternion.LookRotation(direct);
         }
+       
     }
+
+    
+
+
+
 }
