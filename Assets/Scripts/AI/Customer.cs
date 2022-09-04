1cf784b0ecf6bc2af338743cdb3de04910150edc
00Jul1an00 <mrjulik.beletsky@yandex.ru>
2022-09-04 22:31:51 +0300

    2

--- Assets/Scripts/AI/Customer.cs
+++ Assets/Scripts/AI/Customer.cs
@@ -15,7 +15,7 @@ public class Customer : MonoBehaviour
     {
         if(sender is CustomerSpawner)
         {
-            _wishListCapacity = Random.Range(1, _wishListMaxCapacity); //_wishListMaxCapacity + 1
+            _wishListCapacity = Random.Range(1, _wishListMaxCapacity);
             RandomWishList();
             WishList = _wishList;
         }
