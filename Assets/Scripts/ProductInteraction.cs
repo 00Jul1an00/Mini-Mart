170b2088f8f3828e653c8fc2e8600e6225cd36bb
Bonifa32 <Salmon3221@mail.ru>
2022-09-17 23:44:31 +0300

    1

--- Assets/Scripts/ProductInteraction.cs
+++ Assets/Scripts/ProductInteraction.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ITakable : MonoBehaviour
+public class ProductInteraction : MonoBehaviour
 {
     private bool _isHold = false;
     private float _distance = 1f;
     private RaycastHit2D _hit;
-    private void TakeProduct()
+    private void TakeProduct(Collider col)
     {
         if (!_isHold)
         {
             Physics2D.queriesStartInColliders = false;
             _hit = Physics2D.Raycast(transform.position, transform.forward, _distance);
+
+            if(_hit.collider == col)
+            {
+
+            }
         }
     }
 
